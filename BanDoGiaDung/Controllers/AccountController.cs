94ed3707f108ae6025a09cd253d2d34bbac8784d
NgocQuy <hnquuy08@gmail.com>
2025-11-14 20:45:14 +0700

    account

---
+++ BanDoGiaDung/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using BanDoGiaDung.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanDoGiaDung.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        private  GiaDungDbContext db = new GiaDungDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string returnUrl)
+        {
+            if (String.IsNullOrEmpty(returnUrl) && Request.UrlReferrer != null && Request.UrlReferrer.ToString().Length > 0)
+            {
+                return RedirectToAction("Login", new { returnUrl = Request.UrlReferrer.ToString() });
+            }
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+}
