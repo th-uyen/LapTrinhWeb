83cee71668442fa0dd583bd08ff9a77dcd3de59e
HoaiNam765 <hoainam070605a@gmail.com>
2025-11-22 21:34:09 +0700

    Them Anh Products va Anh Brand + sua lai dia chi cua image products

---
+++ BanDoGiaDung/Migrations/202511210845275_ThemCotAnhChoBrand.cs
@@ -0,0 +1,18 @@
+﻿namespace BanDoGiaDung.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ThemCotAnhChoBrand : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Brand", "image", c => c.String(maxLength: 200));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Brand", "image");
+        }
+    }
+}
